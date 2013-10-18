13c571f5c7fed3f565e075029248d09e1efb2cba
Curtis Loisel <curtis@voceconnect.com>
2013-10-18 13:14:18 -0400

    Adding readme and updating plugin name references

---
+++ README.md
@@ -0,0 +1,38 @@
+Voce Media Setting
+===================
+Contributors: prettyboymp, kevinlangleyjr, jeffstieler, markparolisi, banderon
+Tags: post, meta
+Requires at least: 3.5
+Tested up to: 3.6
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+## Description
+Voce Settings API Extension for creating media settings.
+
+## Installation
+
+### As standard plugin:
+> See [Installing Plugins](http://codex.wordpress.org/Managing_Plugins#Installing_Plugins).
+
+### As theme or plugin dependency:
+> After dropping the plugin into the containing theme or plugin, add the following:
+```php
+if( ! class_exists( 'Voce_Media_Setting' ) ) {
+    require_once( $path_to_voce_media_setting . '/voce-media-setting.php' );
+}
+```
+
+## Usage
+
+#### Example
+
+```php
+<?php
+
+?>
+```
+
+**1.0**
+*Initial version.*
---
+++ voce-media-setting.php
@@ -0,0 +1,108 @@
+<?php
+
+if ( !class_exists( 'Voce_Media_Setting' ) ){
+
+	class Voce_Media_Setting {
+		public static function initialize(){
+
+			add_action( 'admin_enqueue_scripts', function($hook){
+				$allowed_hooks = apply_filters('voce-media-settings-js-hooks', array());
+				if( 'settings_page_' == substr( $hook, 0, 14 ) || in_array($hook, $allowed_hooks) ) {
+					wp_enqueue_media();
+					wp_enqueue_script('voce-media-setting-js', plugins_url( '/js/voce-media-setting.js', __FILE__ ), array( 'jquery', 'media-upload', 'wp-ajax-response' ) );
+					wp_enqueue_style( 'voce-media-setting-css', plugins_url( '/css/voce-media-setting.css', __FILE__ ) );
+				}
+			});
+
+		}
+
+		public static function display_media_select( $value, $setting, $args ){
+			self::render_html( $value, $setting, $args );
+			if(!empty($args['description']))
+				printf('<span class="description">%s</span>', $args['description']);
+		}
+
+		public static function render_html( $value, $setting, $args, $return = false ){
+
+			$default_args = array(
+				'mime_types'      => array( 'image' ),
+				'multiple_select' => false,
+				'thumb_size'      => 'medium'
+			);
+			$args = shortcode_atts( $default_args, $args );
+			extract($args);
+
+			// Html content vars
+			$field_id     = $setting->get_field_id();
+			$field_name   = $setting->get_field_name();
+			$label_add    = 'Set ' . $setting->title;
+			$label_remove = 'Remove ' . $setting->title;
+			$link_content = '';
+			$hide_remove  = true;
+
+			// If value is set get thumbnails to display and show remove button
+			if ( $value ) {
+				$values = explode(',', $value);
+				foreach ( $values as $attachment ) {
+					$value_post = get_post($attachment);
+					if ( $value_post ) {
+						$mime_type = $value_post->post_mime_type;
+						$icon = ( strpos( $mime_type, 'image' ) ) ? false : true;
+						$thumbnail_html = wp_get_attachment_image( $attachment, $thumb_size, $icon );
+						if ( ! empty( $thumbnail_html ) ) {
+							$link_content .= $thumbnail_html;
+							$hide_remove = false;
+						}
+					}
+				}
+			}
+
+			// If no thumbnails then use link text and hide remove
+			if ( empty($link_content) ) {
+				$link_content = esc_html($label_add);
+				$hide_remove = true;
+			}
+
+			// Settings for the the js object
+			$field_settings = array(
+				'thumbSize' => $thumb_size,
+				'modalOptions' => array(
+					'multiple' => $multiple_select,
+					'title' => $label_add,
+					'button' => array(
+						'text' => $label_add
+					),
+					'library' => array(
+						'type' => $mime_types
+					)
+				)
+			);
+
+			wp_get_mime_types()
+
+		?>
+			<div class="voce-media-setting hide-if-no-js" data-field-settings="<?php echo esc_attr(json_encode($field_settings)); ?>" >
+				<p>
+					<input class="hidden vpm-id" type="hidden" id="<?php echo esc_attr( $field_id ); ?>" name="<?php echo esc_attr( $field_name ); ?>" value="<?php echo esc_attr( $value ); ?>" />
+					<a title="<?php echo esc_attr( $label_add ); ?>" href="#" class="vpm-add <?php echo ( $hide_remove ) ? 'button' : ''; ?>">
+						<?php echo $link_content; ?>
+					</a>
+				</p>
+				<p>
+					<a href="#" class="button vpm-remove <?php echo ( $hide_remove ) ? 'hidden' : ''; ?>">
+						<?php echo esc_html( $label_remove ); ?>
+					</a>
+				</p>
+			</div>
+		<?php
+		}
+
+		public static function sanitize_media_select_callback($value, $setting, $args){
+			$values = explode(',', $value);
+			array_map( 'intval', $values);
+			return implode(',', $values);
+		}
+	}
+	add_action( 'init', array( 'Voce_Media_Setting', 'initialize' ) );
+
+}
